ba980cab62eb0b3c96cb373d5de8c8670a47e144
XiongAmao <onycat@live.cn>
2019-10-08 17:59:54 +0800

    create demo.

---
+++ index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="App-container">
+    There is no error here
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {
+
+    },
+    props: {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .App-container {
+
+  }
+</style>
